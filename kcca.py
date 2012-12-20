d84e0c6868d4a3814ecc29cefda78772d0d16815
lorenzoriano <lorenzo.riano@berkeley.edu>
2012-12-20 14:04:19 -0800

    Added features centering

--- kcca.py
+++ kcca.py
@@ -1,4 +1,5 @@
 import numpy
+from numpy import dot, eye, ones
 import scipy.linalg
 
 class GaussianKernel(object):
@@ -30,13 +31,13 @@ class GaussianKernel(object):
         if X1.shape[1] != X2.shape[1]:
             raise ValueError("Invalid matrix dimentions: " + str(X1.shape) + " " + str(X2.shape))
 
-        j1 = numpy.ones((X1.shape[0], 1))
-        j2 = numpy.ones((X2.shape[0], 1))
+        j1 = ones((X1.shape[0], 1))
+        j2 = ones((X2.shape[0], 1))
 
         diagK1 = numpy.sum(X1**2, 1)
         diagK2 = numpy.sum(X2**2, 1)
 
-        X1X2 = numpy.dot(X1, X2.T)
+        X1X2 = dot(X1, X2.T)
 
         Q = (2*X1X2 - numpy.outer(diagK1, j2) - numpy.outer(j1, diagK2) )/ (2*self.sigma**2)
 
@@ -45,17 +46,18 @@ class GaussianKernel(object):
     def __str__(self):
         return "GaussianKernel: sigma = " + str(self.sigma)
 
-class LinearKernel(object):
+class PolyKernel(object):
     """
     A class to find linear kernel evaluations k(x, y) = <x, y> 
     """
-    def __init__(self):
+    def __init__(self, c, p):
         """
         Intialise class. 
         """
-        pass
+        self.c = c
+        self.p = p
 
-    def evaluate(self, X1, X2):
+    def __call__(self, X1, X2):
         """
         Find kernel evaluation between two matrices X1 and X2 whose rows are
         examples and have an identical number of columns.
@@ -71,7 +73,11 @@ class LinearKernel(object):
         if X1.shape[1] != X2.shape[1]:
             raise ValueError("Invalid matrix dimentions: " + str(X1.shape) + " " + str(X2.shape))
 
-        return numpy.dot(X1, X2.T)
+        return (dot(X1, X2.T) + self.c) ** self.p
+
+class LinearKernel(PolyKernel):
+    def __init__(self):
+        super(LinearKernel, self).__init__(0, 1)
     
 class KCCA(object):
     def __init__(self, kernel1, kernel2, regularization, method = 'kettering_method'):
@@ -88,15 +94,15 @@ class KCCA(object):
     def standard_hardoon_method(self, K1, K2, reg):
         
         N = K1.shape[0]
-        I = numpy.eye(N)
+        I = eye(N)
         Z = numpy.zeros((N,N))    
         
-        R1 = numpy.c_[Z, numpy.dot(K1, K2)]
-        R2 = numpy.c_[numpy.dot(K2, K1), Z]
+        R1 = numpy.c_[Z, dot(K1, K2)]
+        R2 = numpy.c_[dot(K2, K1), Z]
         R =  numpy.r_[R1, R2]
         
-        D1 = numpy.c_[numpy.dot(K1, K1 + reg*I), Z]
-        D2 = numpy.c_[Z, numpy.dot(K2, K2 + reg*I)]
+        D1 = numpy.c_[dot(K1, K1 + reg*I), Z]
+        D2 = numpy.c_[Z, dot(K2, K2 + reg*I)]
         D = 0.5*numpy.r_[D1, D2]
         
         return (R, D)    
@@ -104,7 +110,7 @@ class KCCA(object):
     def simplified_hardoon_method(self, K1, K2, reg):
         
         N = K1.shape[0]
-        I = numpy.eye(N)
+        I = eye(N)
         Z = numpy.zeros((N,N))    
         
         R1 = numpy.c_[Z, K2]
@@ -120,7 +126,7 @@ class KCCA(object):
     def kettering_method(self, K1, K2, reg):
         
         N = K1.shape[0]
-        I = numpy.eye(N)
+        I = eye(N)
         Z = numpy.zeros((N,N))    
         
         R1 = numpy.c_[K1, K2]
@@ -135,10 +141,12 @@ class KCCA(object):
 
     def kcca(self, K1, K2):
         
+        #remove the mean in features space
         N = K1.shape[0]
-        N0 = numpy.eye(N) - 1./N * numpy.ones(N)
-        K1 = numpy.dot(numpy.dot(N0,K1),N0)
-        K2 = numpy.dot(numpy.dot(N0,K2),N0)    
+        N0 = eye(N) - 1./N * ones(N)
+        K1 = dot(dot(N0,K1),N0)
+        K2 = dot(dot(N0,K2),N0)
+        
         R, D = self.method(K1, K2, self.reg)
         
         #solve generalized eigenvalues problem
@@ -152,11 +160,11 @@ class KCCA(object):
         alpha1 = alpha[:N]
         alpha2 = alpha[N:]
         
+        y1 = dot(K1, alpha1)
+        y2 = dot(K2, alpha2)
+
         self.alpha1 = alpha1
         self.alpha2 = alpha2
-        
-        y1 = numpy.dot(K1, alpha1)
-        y2 = numpy.dot(K2, alpha2)
         
         return (y1, y2, beta)        
         
@@ -164,24 +172,53 @@ class KCCA(object):
         self.trainX1 = X1
         self.trainX2 = X2
         
-        K1 = self.kernel1(X1, X1)
-        K2 = self.kernel2(X2, X2)
+        self.K1 = self.kernel1(X1, X1)
+        self.K2 = self.kernel2(X2, X2)
         
-        self.kcca(K1, K2)
+        (y1, y2, beta) = self.kcca(self.K1, self.K2)
+        self.y1_ = y1
+        self.y2_ = y2
+        self.beta_ = beta
         return self
     
     def transform(self, X1 = None, X2 = None):
         rets = []
         if X1 is not None:
-            testTrainK1 = self.kernel1(X1, self.trainX1)
-            rets.append(numpy.dot(testTrainK1, self.alpha1))
+            Ktest = self.kernel1(X1, self.trainX1)            
+            K = self.K1
+            
+            L, M = Ktest.shape
+            ones_m = ones((M, M))
+            ones_mp = ones((L, M)) / M
+            
+            #taken from:
+            # Scholkopf, B., Smola, A., & Muller, K. R. (1998).
+            # Nonlinear component analysis as a kernel eigenvalue problem.
+            # Neural computation, 10(5), 1299-1319.
+            K1 = (Ktest - dot(ones_mp, K)
+                  - dot(Ktest, ones_m) + dot(dot(ones_mp, K), ones_m)
+                  )
+            
+            res1 =  dot(K1, self.alpha1)
+            rets.append(res1)
             
         if X2 is not None:
-            testTrainK2 = self.kernel1(X2, self.trainX2)
-            rets.append(numpy.dot(testTrainK2, self.alpha2))
+            Ktest = self.kernel1(X2, self.trainX2)            
+            K = self.K2
             
-        return rets
-    
+            L, M = Ktest.shape
+            ones_m = ones((M, M))
+            ones_mp = ones((L, M)) / M
             
+            #taken from:
+            # Scholkopf, B., Smola, A., & Muller, K. R. (1998).
+            # Nonlinear component analysis as a kernel eigenvalue problem.
+            # Neural computation, 10(5), 1299-1319.
+            K2 = (Ktest - dot(ones_mp, K)
+                  - dot(Ktest, ones_m) + dot(dot(ones_mp, K), ones_m)
+                  )
             
+            res2 =  dot(K2, self.alpha2)
+            rets.append(res2)
             
+        return rets            
